c5147fa17071010773dca1f32f64f4a3f5eb0ff7
Minh-Duc Phan <gammphan@gmail.com>
2025-04-16 14:45:38 +0200

    Commande dans modele

--- Modele/Commande.java
+++ Modele/Commande.java
@@ -1,4 +1,28 @@
 package Modele;
 
 public class Commande {
+    private int id;
+    private int id_client;
+    private int id_produit;
+    private int quantite;
+    private double prix;
+    private String date;
+
+    public Commande (int id, int id_client, int id_produit, int quantite, float prix, String date) {
+        this.id = id;
+        this.id_client = id_client;
+        this.id_produit = id_produit;
+        this.quantite = quantite;
+        this.prix = prix;
+        this.date = date;
+    }
+
+    public int getId(){ return this.id;}
+    public int getId_client(){ return this.id_client;}
+    public int getId_produit(){ return this.id_produit;}
+    public int getQuantite(){ return this.quantite;}
+    public double getPrix(){ return this.prix;}
+    public String getDate(){ return this.date;}
 }
+
+
