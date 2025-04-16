1bf7ff2bc79f647f2222701864634b9c62608f3a
Minh-Duc Phan <gammphan@gmail.com>
2025-04-16 14:18:48 +0200

    Test DataBaseCOnnection et ProduitDAO

---
+++ Dao/ProduitDAO.java
@@ -0,0 +1,12 @@
+package Dao;
+
+import java.util.List;
+import Archive.Modele.Produit;
+
+public interface ProduitDAO {
+    Produit getById(int id);
+    List<Produit> getAll();
+    boolean insert(Produit produit);
+    boolean update(Produit produit);
+    boolean delete(int id);
+}
