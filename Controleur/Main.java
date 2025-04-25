621cffdce14c6e2edc2806ce1b218fe1fb3f7da5
Jerry CHENG <jrrychg@gmail.com>
2025-04-25 22:47:05 +0200

    Correction main.java en main dans Controller

---
+++ Controleur/Main.java
@@ -0,0 +1,20 @@
+package Controleur;
+
+import javax.swing.*;
+import java.awt.*;
+import Vue.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Interface Shopping");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(800, 600));
+        frame.setLayout(new BorderLayout());
+
+        Connexion menuBar = new Connexion(frame);
+        frame.setJMenuBar(menuBar);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
