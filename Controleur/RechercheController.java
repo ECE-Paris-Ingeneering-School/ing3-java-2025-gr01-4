3fbeb9c8edad83b74746ccad0c486da7f53393b0
BENOITWilliam <sowgan.william@gmail.com>
2025-04-27 13:56:53 +0200

    Correction bug (bar de navigation qui ne marchait pas dans le Panel ProduitPanel)

--- Controleur/RechercheController.java
+++ Controleur/RechercheController.java
@@ -2,6 +2,7 @@ package Controleur;
 
 import DAO.ProduitDAOImpl;
 import Vue.ProduitPanel;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -13,13 +14,31 @@ public class RechercheController {
     }
 
     public void afficherProduitsParRecherche(String recherche) {
+        /*
         ProduitPanel produitPanel = new ProduitPanel(new ProduitDAOImpl().getByRecherche(recherche));
-        JScrollPane scrollPane = new JScrollPane(produitPanel);
 
-        // 🔥 Ajouter un nouvel écran sans tout enlever
-        conteneurPrincipal.add(scrollPane, "ResultatRecherche");
+        System.out.println(produitPanel);
 
-        CardLayout cl = (CardLayout) (conteneurPrincipal.getLayout());
-        cl.show(conteneurPrincipal, "ResultatRecherche");
+        // Nettoyer le contenu existant
+        conteneurPrincipal.removeAll();
+        conteneurPrincipal.add(new JScrollPane(produitPanel));
+        conteneurPrincipal.revalidate();
+        conteneurPrincipal.repaint();
+
+         */
+        ProduitPanel produitPanel = new ProduitPanel(new ProduitDAOImpl().getByRecherche(recherche));
+
+        Component composant = conteneurPrincipal.getComponent(0);
+        if (composant instanceof JPanel) {
+            JPanel panel = (JPanel) composant;
+            panel.removeAll();
+            // Ajouter une nouvelle carte temporaire pour la catégorie sélectionnée
+            conteneurPrincipal.add(new JScrollPane(produitPanel), "CategorieSelection");
+
+            CardLayout cl = (CardLayout) conteneurPrincipal.getLayout();
+            cl.show(conteneurPrincipal, "CategorieSelection");
+            panel.revalidate();
+            panel.repaint();
+        }
     }
 }
